136df5ffb3583d67f7c8e7bdac12c2622e6fe94f
Kaitlin McArdle <kmcardle@csm-pm-38123-20.local>
2013-05-07 15:59:39 -0700

    midterm time

---
+++ Midterm/Electrostatics.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def pointPotential(x,y,q,posx,posy):
+    """finds the electric potential at a point due to point charge"""
+    k = 1.3806488e-23 #Nm^2/C^2
+    V = (k*q/(np.sqrt((x-posx)**2+(y-posy)**2)))
+    return V
+
+def dipolePotential(x,y,q,d):
+    """finds the electric potential at a point given a two point charges"""
+    Vxy = pointPotential(x,y,q,d/2,0) + pointPotential(x,y,-q,-d/2,0)
+    return Vxy
+
+def pointField(x,y,q,Xq,Yq):
+    """Returns a tuple of the electric field caused by q at (Xq,Yq)
+		(x,y) 	: the arrays th area
+		q	: the charge
+		(Xq,Yq)	: The loation of the point charge
+
+		Returns [Ex,Ey]"""
+    k = 8.987551787e9 #Nm^2/C^2
+    Ex = (k*q*(x-Xq))/(((x-Xq)**2.+(y-Yq)**2.)**(0.5))
+    Ey = (k*q*(y-Yq))/(((x-Xq)**2.+(y-Yq)**2.)**(0.5))
+    return [Ex,Ey]
